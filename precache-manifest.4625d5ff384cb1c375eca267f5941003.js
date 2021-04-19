559c1a973376648b03af97d4fb6f7555ae310927
Chintan-K <kalsariachintan@gmail.com>
2021-04-19 20:27:11 +0530

    Updates

---
+++ precache-manifest.4625d5ff384cb1c375eca267f5941003.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c544a77064194a99adea90cb1c4a646b",
+    "url": "/netflix-cards/index.html"
+  },
+  {
+    "revision": "0d689a71164507a1abe7",
+    "url": "/netflix-cards/static/css/main.d9aa8bb7.chunk.css"
+  },
+  {
+    "revision": "f0ba0e24f383fdc813bb",
+    "url": "/netflix-cards/static/js/2.135830a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/netflix-cards/static/js/2.135830a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d689a71164507a1abe7",
+    "url": "/netflix-cards/static/js/main.48e790b3.chunk.js"
+  },
+  {
+    "revision": "79e086c780c8d9e7b1fe",
+    "url": "/netflix-cards/static/js/runtime-main.be3bf27a.js"
+  }
+]);
